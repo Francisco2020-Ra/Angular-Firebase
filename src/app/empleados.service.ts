f5185665ece7d0bad7a302808a1359747887490e
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-10-19 19:28:50 -0300

    add project

---
+++ src/app/empleados.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Empleado } from './empleado/empleado.model';
+import { ServicioEmpleadosService } from './servicio-empleados.service';
+
+@Injectable()
+export class EmpleadosService {
+
+  constructor(private serviceEmpleadoService:ServicioEmpleadosService){}
+
+  empleados: Empleado[] = [
+      new Empleado("Nicolas", "Gonzalez", "Presidente", 7500),
+      new Empleado("Maria", "Martín", "Vicepresidente", 6500),
+      new Empleado("Ana", "Lopez", "Jefa de Area", 4500),
+      new Empleado("Alfredo", "Gimenez", "Supervisor", 3500),
+  ];
+
+
+  agregarEmpleadoServicio(empleado: Empleado) {
+      this.serviceEmpleadoService.muestraMensaje("Persona a agregar: " + empleado.nombre);
+      this.empleados.push(empleado);
+  }
+
+  encontrarEmpleado(indice:number){
+      let empleado:Empleado = this.empleados[indice];
+
+      return empleado;
+  }
+
+  actualizarEmpleado(indice:number, empleado:Empleado){
+      let empleadoModificado = this.empleados[indice];
+
+      empleadoModificado.nombre = empleado.nombre;
+      empleadoModificado.apellido = empleado.apellido;
+      empleadoModificado.cargo = empleado.cargo;
+      empleadoModificado.salario = empleado.salario;
+  }
+
+  eliminarEmpleado(indice:number){
+
+      this.empleados.splice(indice,1);
+  }
+}
---
+++ src/app/home-component/home-component.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Empleado } from '../empleado/empleado.model';
+import { EmpleadosService } from '../empleados.service';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
+
+@Component({
+  selector: 'app-home-component',
+  templateUrl: './home-component.component.html',
+  styleUrls: ['./home-component.component.css']
+})
+export class HomeComponentComponent {
+
+  titulo = "Listado de Empleados"
+
+  constructor(
+    private servicioEmpleado:ServicioEmpleadosService,
+    private servicioDataEmpleado: EmpleadosService){
+
+      this.empleados = this.servicioDataEmpleado.empleados;
+    }
+
+
+    empleados:Empleado[] = [];
+
+    agregarEmpleado(){
+      let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido,this.cuadroCargo,this.cuadroSalario)
+  
+      //this.servicioEmpleado.muestraMensaje("Nombre del empleado: " + miEmpleado.nombre);
+  
+      this.servicioDataEmpleado.agregarEmpleadoServicio(miEmpleado);
+  
+    }
+    cuadroNombre: string = "";
+    cuadroApellido: string = ""
+    cuadroCargo: string = "";
+    cuadroSalario: number = 0;
+
+
+}
